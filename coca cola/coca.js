a8bb79fac7d035f7d68c04b6a9cecf68121ec6e0
alumno <alumno@Instituto.local>
2023-11-27 10:45:12 -0300

    CAMBIOS

--- coca cola/coca.js
+++ coca cola/coca.js
@@ -5,5 +5,16 @@ let botonCambiarTexto = document.querySelector('#coca')
 
 botonCambiarTexto.onclick = function(){
     texto.style.color = 'white'
-    texto.innerText = 'Solo voy a aprobar a vicky y clari'
+    texto.innerText = 'Diferentes cocas'
+    function cambiarImagen() {
+        if(cambio == false) {
+         coca.src = 'https://www.coca-cola.com/content/dam/onexp/es/es/products/coca-cola-zero/es_coca-cola-zero_product_750.jpg'
+         coca.alt = 'Coca Cero';
+         cambio = true
+       }else
+       {
+         coca.src = 'https://bebicash.com/wp-content/uploads/5449000000996-24.jpg'
+       cambio  = false
+       }
+       }
 }
